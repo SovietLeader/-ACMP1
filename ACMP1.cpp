264131132672c7ec9570f2959f37d5556f0e6673
SovietLeader <72862199+SovietLeader@users.noreply.github.com>
2020-10-28 18:29:50 +0700

    Add files via upload

---
+++ ACMP1.cpp
@@ -0,0 +1,15 @@
+﻿// Задаются координаты центрв и радиусы окружностей, определить, пересекаются ли они 
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	long doule x1, y1, x2, y2, r1 ,r2; cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+	long doule;
+	a = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	if ((r1 + r2) >= a) cout << "YES";
+	else cout << "NO";
+
+	return(0);
+}
